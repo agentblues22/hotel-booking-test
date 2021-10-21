e12ab0ebfcbaf6d00ee02303002130073cbfb07b
agentblues22 <georgyroy007@gmail.com>
2021-10-21 12:16:12 +0530

    latest stable change

--- pages/discover.js
+++ pages/discover.js
@@ -9,6 +9,9 @@ export default function Discover() {
       description:
         "The standard room features vibrant décor, combining executive furnishings with twin bed and ample closet space.",
       heading: "Standard Room",
+      area: "160 sq.ft",
+      tariff: "1480 Incl of taxes",
+      max_occ: "2",
       id: "ph1",
       url: [
         {
@@ -26,6 +29,9 @@ export default function Discover() {
       description:
         "Deluxe room features a super comfy twin beds draped in crisp white linen, a seating area and a comfortable workspace. Each room has been designed and furnished attractively. ",
       heading: "Deluxe Room",
+      area: "200 sq.ft",
+      tariff: "1680 Incl of taxes",
+      max_occ: "2",
       id: "ph2",
       url: [
         {
@@ -41,8 +47,11 @@ export default function Discover() {
     },
     {
       description:
-        ": The Executive rooms give you a space where you can relax your every second. It is a spacious room with a two twin size beds, a dedicated seating area and stylishly designed with detail to make your stay effortless and luxurious.",
+        "The Executive rooms give you a space where you can relax your every second. It is a spacious room with a two twin size beds, a dedicated seating area and stylishly designed with detail to make your stay effortless and luxurious.",
       heading: "Executive Room",
+      area: "280 sq.ft",
+      tariff: "2520 Incl of taxes",
+      max_occ: "3",
       id: "ph3",
       url: [
         {
@@ -72,9 +81,22 @@ export default function Discover() {
                 <div>{photo.heading}</div>
               </div>
               <div className={classes.room_paragraph}>{photo.description}</div>
-              <div className={classes.room_misc}>
-                <Icongallery />
+              <div>
+                <p className={classes.stats}>
+                  <span className={classes.spantext}>Space size:</span>
+                  {photo.area}
+                </p>
+                <p className={classes.stats}>
+                  <span className={classes.spantext}>Max Occupancy:</span>
+                  {photo.max_occ}
+                </p>
+                <p className={classes.stats}>
+                  <span className={classes.spantext}>Tariff:</span>
+                  {photo.tariff}
+                </p>
               </div>
+
+              <Icongallery />
             </div>
           </div>
         );
