87d15ab7a94218c707e681ec424cd5addcdcd6f4
agentblues22 <georgyroy007@gmail.com>
2021-10-22 17:26:14 +0530

    rolled back to stability

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,50 +1,32 @@
 import classes from "./MainNavigation.module.css";
 import NextLink from "next/link";
-import React from "react";
 
-function makeCheck(e) {
-  this.refs.checkbox.checked = false;
-}
+function MainNavigation() {
+  return (
+    <section className={classes.container}>
+      <div>
+        <button className={classes.item_left}>logo</button>
+      </div>
+      <div className="nav">
+        <label className={classes.label} htmlFor="toggle">
+          &#9776;
+        </label>
+        <input className={classes.toggle} type="checkbox" id="toggle" />
+        <div className={classes.menu}>
+          <NextLink href="/" passHref>
+            <a className={classes.menu_links}>Home</a>
+          </NextLink>
 
-class MainNavigation extends React.Component {
-  render() {
-    return (
-      <section className={classes.container}>
-        <div>
-          <button className={classes.item_left}>logo</button>
+          <NextLink href="/discover" passHref>
+            <a className={classes.menu_links}>Discover</a>
+          </NextLink>
+          <NextLink href="/contact" passHref>
+            <a className={classes.menu_links_contact}>Contact Us</a>
+          </NextLink>
         </div>
-        <div className="nav">
-          <label className={classes.label} htmlFor="toggle">
-            &#9776;
-          </label>
-          <input
-            className={classes.toggle}
-            type="checkbox"
-            ref="checkbox"
-            id="toggle"
-          />
-          <div className={classes.menu}>
-            <NextLink href="/" passHref>
-              <a className={classes.menu_links} onClick={makeCheck}>
-                Home
-              </a>
-            </NextLink>
-
-            <NextLink href="/discover" passHref>
-              <a className={classes.menu_links} onClick={makeCheck}>
-                Discover
-              </a>
-            </NextLink>
-            <NextLink href="/contact" passHref>
-              <a className={classes.menu_links_contact} onClick={makeCheck}>
-                Contact Us
-              </a>
-            </NextLink>
-          </div>
-        </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default MainNavigation;
